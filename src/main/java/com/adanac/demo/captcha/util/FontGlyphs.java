1927debdf2521d219f871c8dcb5419c26789dbd1
adanac <adanac@sina.com>
2016-05-31 17:50:08 +0800

    init files

---
+++ src/main/java/com/adanac/demo/captcha/util/FontGlyphs.java
@@ -0,0 +1,11 @@
+package com.adanac.demo.captcha.util;
+
+public class FontGlyphs {
+	public String c;
+	public int weight;
+
+	public FontGlyphs(String c, int weight) {
+		this.c = c;
+		this.weight = weight;
+	}
+}
